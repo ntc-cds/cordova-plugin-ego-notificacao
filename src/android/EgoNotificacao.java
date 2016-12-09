0bb63bdfe38c08797781037bd37a15b12112bc16
Jhonatas <jhonatasmartins@hotmail.com>
2016-12-09 15:51:35 -0200

    fix

--- src/android/EgoNotificacao.java
+++ src/android/EgoNotificacao.java
@@ -18,143 +18,56 @@ import android.net.Uri;
 import android.os.Handler;
 
 
-public class EGONotificaService extends Service {
-
-    static final long TIMER = 5 * 60 * 1000;
-    private Handler handler;
+public class EgoNotificacao extends CordovaPlugin {
 
     @Override
-    public IBinder onBind(Intent arg0) {
-        return null;
-    }
-
-    static String url = null;
-    static MyLoopService mls = null;
-    static boolean run = false;
-    static int sessao = -1;
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        handler = new Handler();
-
-        if (mls == null) {
-            run = true;
-            mls = new MyLoopService();
-            mls.start();
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("initService")) {
+            String message = args.getString(0);
+            this.initService(message, callbackContext);
+            return true;
+        } else if (action.equals("ativar")) {
+            String message = args.getString(0);
+            this.ativar(message, callbackContext);
+        } else if (action.equals("desativar")) {
+            this.desativar(callbackContext);
         }
-        return super.onStartCommand(intent, flags, startId);
+        return false;
     }
 
-    static public void setURL(String url) {
-        EGONotificaService.url = url;
+    private void initService(String message, CallbackContext callbackContext) {
+        android.content.Context context = this.cordova.getActivity().getApplicationContext();
+        context.startService(new android.content.Intent(context, EGONotificaService.class));
+        (new android.media.ToneGenerator(android.media.AudioManager.STREAM_NOTIFICATION, 100)).startTone(android.media.ToneGenerator.TONE_CDMA_PIP, 150);
+        if (message != null && message.length() > 0) {
+            EGONotificaService.setURL(message);
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Parâmetro esperado.");
+        }
     }
 
-    static public void ativar(int sessao) {
-        EGONotificaService.sessao = sessao;
-        EGONotificaService.run = true;
-    }
-
-    static public void desativar() {
-        EGONotificaService.run = false;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    class MyLoopService extends Thread {
-        protected Notifica getData() throws IOException {
-            if (EGONotificaService.sessao < 0) {
-                return null;
-            }
+    private void ativar(String message, CallbackContext callbackContext) {
+        int sessao;
+        if (message != null && message.length() > 0) {
             try {
-                Log.i("EGOSvc2", "Try load url "+EGONotificaService.url + "/cet/notificastatus/" + EGONotificaService.sessao);
-                URL url = new URL(EGONotificaService.url + "/cet/notificastatus/" + EGONotificaService.sessao);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setUseCaches(false);
-                connection.connect();
-                int status = connection.getResponseCode();
-                switch (status) {
-                    case 200:
-                    case 201: {
-                        InputStream response = connection.getInputStream();
-                        BufferedReader br = new BufferedReader(new InputStreamReader(response));
-                        JsonReader jr = new JsonReader(br);
-                        return readNotifica(jr);
-                    }
-                    default: {
-                        return null;
-                    }
-                }
-            } catch (Throwable e) {
-                return null;
+                sessao = Integer.parseInt(message);
+                EGONotificaService.ativar(sessao);
+                callbackContext.success(message);
+            } catch (NumberFormatException e) {
+                callbackContext.error("Parâmetro inválido.");
             }
-        }
-
-        protected Notifica readNotifica(JsonReader jr) throws IOException {
-            boolean retorno = false;
-            jr.beginObject();
-            while (jr.hasNext()) {
-                String nextName = jr.nextName();
-                Log.i("EGOSvc2", nextName);
-                if (nextName.equalsIgnoreCase("retorno")) {
-                    retorno = jr.nextBoolean();
-                }
-            }
-            return new Notifica(retorno);
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            android.os.Looper.prepare();
-            while (true) {
-                try {
-                    if (run) {
-                        Notifica notifica = getData();
-                        if ((notifica != null) && notifica.retorno) {
-                            heads_up_notify();
-                        }
-                    }
-                } catch (Throwable e) {
-                }
-                try {
-                    Thread.sleep(TIMER);
-                } catch (Throwable e) {
-                }
-            }
+        } else {
+            callbackContext.error("Parâmetro esperado.");
         }
     }
 
-    void heads_up_notify() {
+    private void desativar(CallbackContext callbackContext) {
         try {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(EGONotificaService.this,"eGO",Toast.LENGTH_LONG).show();
-                }
-            });
-
-            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r = RingtoneManager.getRingtone(this, notification);
-            r.play();
+            EGONotificaService.desativar();
+            callbackContext.success("ok");
         } catch (Throwable e) {
-            e.printStackTrace();
-        }
-    }
-
-    class Notifica {
-        private boolean retorno;
-
-        public Notifica(boolean retorno) {
-            this.retorno = retorno;
-        }
-
-        public boolean isRetorno() {
-            return retorno;
+            callbackContext.error(e.getMessage());
         }
     }
 }
