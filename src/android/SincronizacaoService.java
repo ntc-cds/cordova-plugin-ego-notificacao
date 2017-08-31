cc2b7a558957e814c8ab4ed4ddfb1646fbc41549
Jhonatas Santos <jhonatasmartins@hotmail.com>
2017-08-31 11:35:01 -0300

    sincronizacao fix

--- src/android/SincronizacaoService.java
+++ src/android/SincronizacaoService.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
