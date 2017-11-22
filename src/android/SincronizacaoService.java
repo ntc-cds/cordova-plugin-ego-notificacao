89f2d0724d7f139f95de9fa57f1e36b57dff2143
Jhonatas Santos <jhonatasmartins@hotmail.com>
2017-11-22 18:56:57 -0200

    fix

--- src/android/SincronizacaoService.java
+++ src/android/SincronizacaoService.java
@@ -76,7 +76,7 @@ public class SincronizacaoService extends IntentService{
 
     private String getSincronizacaoUrl(Long lastUpdate){
         String url = SINC_PARCIAL;
-        if(lastUpdate == 0 || lastUpdate == null){
+        if(lastUpdate == null){
             return SERVICE_URL + url + "?ultima_sincronizacao=null";
         }
         return SERVICE_URL + url + "?ultima_sincronizacao=" + lastUpdate;
